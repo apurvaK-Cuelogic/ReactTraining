afd6aae6c2f9d20a3844e97fc6ebb6103ee9f880
apurvak-cuelogic <apurva@perpetualny.com>
2018-05-28 14:48:15 +0530

    Updated burger app with sideDrawer

--- burger-builder-app/src/components/UI/Modal/Modal.js
+++ burger-builder-app/src/components/UI/Modal/Modal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop';
+
 const modal = (props) => (
     <Aux>
         <Backdrop show={props.show} clicked={props.modalClosed}/>
